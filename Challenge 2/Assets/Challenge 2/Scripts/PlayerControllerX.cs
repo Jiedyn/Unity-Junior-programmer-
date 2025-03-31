5c9a75bb1e4fafb3eab6d6d95ce2777d50f4412d
Jie <Masterjiedyn@gmail.com>
2025-03-31 16:12:43 +1100

    Challenge 2 completed - learnt about bool and invoke statements, debugged movement, destroy and prefab scripts

--- Challenge 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Challenge 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,14 +5,27 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    public bool canSpawnDog;
+    private void Start()
+    {
+        canSpawnDog = true;
+    }
 
     // Update is called once per frame
     void Update()
     {
         // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && canSpawnDog == true)
         {
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            canSpawnDog = false;
+
+            Invoke("ResetSpawnDog", 2f);
+            // time.deltaTime script to delay next imput and spam
         }
+    }
+    private void ResetSpawnDog()
+    {
+        canSpawnDog = true;
     }
 }
