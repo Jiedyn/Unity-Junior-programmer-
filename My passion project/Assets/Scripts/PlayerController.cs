e03d96c59bc07b5a17a7d0b8103854ba8cb42eb4
Jie <Masterjiedyn@gmail.com>
2025-04-01 17:01:02 +1100

    completed work - made player models an arrow model and started the playercontroller script

---
+++ My passion project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float horizontalinput;
+    public float Speed = 10f;
+    public float yRange = 3f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+
+        if (transform.position.y > yRange)
+        {
+            transform.position = new Vector3(transform.position.x, yRange, transform.position.z);
+        }
+        else if (transform.position.y < -yRange)
+        {
+            transform.position = new Vector3(transform.position.x, -yRange, transform.position.z);
+        }
+        
+    }
+}
