70856fbe34310d20146106388e0aca7fa1cd4aef
Jie <Masterjiedyn@gmail.com>
2025-04-22 16:00:08 +1000

    Completed work - mod the cube project, writing scripts and installing a new package

---
+++ My project/Assets/ModTheCube/Cube.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public MeshRenderer Renderer;
+
+    void Start()
+    {
+        transform.position = new Vector3(Random.Range(1, 5), Random.Range(1, 5), Random.Range(1, 5));
+        transform.localScale = Vector3.one * 3f;
+        Invoke("ChangeColour", 3);
+    }
+
+    void Update()
+    {
+        transform.Rotate(20.0f * Time.deltaTime, 0.0f, 0.0f);
+    }
+
+    public void ChangeColour()
+    {
+        Material material = Renderer.material;
+
+        gameObject.GetComponent<MeshRenderer>().material.color = new Color(0.5f, 1.0f, 0.3f, 0.4f);
+        Debug.Log("colour");
+    }
+}
